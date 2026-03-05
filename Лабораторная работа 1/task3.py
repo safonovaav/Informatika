2dca908cf825aeb0eb4fdbd2eb14207a7b9a05f0
safonovaav <safonova.av@edu.spbstu.ru>
2026-03-05 11:38:10 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task3.py
@@ -0,0 +1,11 @@
+players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# индекс середины
+middle = len(players) // 2
+# Делим игроков на 2 раные команды
+com_1 = players[:middle]
+com_2 = players[middle:]
+# Вывод
+print(com_1)
+print(com_2)
+
